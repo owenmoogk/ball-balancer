2951614e39e962ca1ce43446952cb560cd22ec52
ahlittle@uwaterloo.ca <ahlittle@uwaterloo.ca>
2025-11-04 19:20:37 -0500

    Add camera matrix and distortion coeffs to settings

--- platform_balancing/simulator/settings.py
+++ platform_balancing/simulator/settings.py
@@ -21,3 +21,15 @@ class Settings:
     DT: float = 0.005
     TARGET_FPS: int = 60
     I_SPHERE: float = (2.0 / 5.0) * BALL_RADIUS**2
+
+    # CAMERA SETTINGS
+    # For Logitech C920 HD Pro Webcam
+    C_MATRIX = np.array([3063.894803087576,0.0,448.9224830647149],
+                        [0.0,2604.697840954291,988.3923029245163],
+                        [0.0, 0.0, 1.0])
+    
+    D_PARAM = np.array([-2.348290496607099,
+                        4.3826225970761605,
+                        -0.18277748843460256,
+                        0.030430158578043043,
+                        -7.934750868203259])
